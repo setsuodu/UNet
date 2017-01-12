4f9c694ec7b9efd94a33923abb77cc1d71916f13
Setsuodu <339131001@qq.com>
2017-01-12 17:11:54 +0800

    Class 1-6

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!isLocalPlayer) return;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Rotate(Vector3.up * h * 120 * Time.deltaTime);
+        transform.Translate(Vector3.forward * v * 3 * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        //这个方法只会在本地角色那里调用，当创建角色时
+        base.OnStartLocalPlayer();
+        GetComponent<MeshRenderer>().material.color = Color.blue;
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward;
+    }
+}
