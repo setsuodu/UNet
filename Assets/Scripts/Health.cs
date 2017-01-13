e780c32acae27b008b90ea253027a8066daa2b0e
Setsuodu <339131001@qq.com>
2017-01-13 11:47:11 +0800

    Class 8-10
    Take Damage, health slider.
    Bug: Bullet sync is different. so the health is different on each
    client.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Death");
+        }
+        healthSlider.value = (float)currentHealth / maxHealth;
+    }
+}
